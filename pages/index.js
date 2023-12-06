c17b4404dad01f0f0063c638cb8742c822ad26ab
IsaKaillany <isakaillany123@gmail.com>
2023-12-06 20:31:11 -0300

    update tela inicial

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
-import { RecipeCards } from "./recipeCards/recipeCards";
 import styles from "./styles/index.module.css";
-
+import Link from 'next/link';
 
 export default function Main() {
     return (
         <div className={styles.pageContainer}>
             <h1>Receitas - Unidade 2</h1>
-            <RecipeCards />
+            <div className={styles.cardsContainer}>
+            <Link href="./recipe-1/recipe-1" className={styles.linkPage}>
+                <div className={styles.card}>
+                    Receita 1
+                </div>
+            </Link>
+            <Link href="./recipe-2/pages/recipe-2" className={styles.linkPage}>
+                <div className={styles.card}>
+                    Receita 2
+                </div>
+            </Link>
+             <Link href="./recipe-3/movies" className={styles.linkPage}>
+                <div className={styles.card}>
+                    Receita 3
+                </div>
+            </Link>
+          {/*  <Link >
+                <div className={styles.card}>
+                    Receita 4
+                </div>
+            </Link> */}
+        </div>
         </div>
     );
 }
