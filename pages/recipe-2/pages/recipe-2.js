6f0cdf6dbf0750704292ab94f69c8383fcd61214
IsaKaillany <isakaillany123@gmail.com>
2023-12-11 21:14:37 -0300

    update

--- pages/recipe-2/pages/recipe-2.js
+++ pages/recipe-2/pages/recipe-2.js
@@ -1,6 +1,6 @@
-import { About } from "./about/about";
+import About from "./about/about";
 import Button from "./button/button";
-import { Img } from "./img/img";
+import Img from "./img/img";
 import NavBar from "./navBar/navBar";
 import styles from "./styles/novaPagina.module.css";
 
@@ -8,8 +8,8 @@ export default function Principal() {
     return (
         <div className={styles.container}>
             <NavBar />
-            {/* <Img />
-            <About /> */}
+            <Img />
+            <About />
             <Button text="Documentação" />
         </div>
     );
